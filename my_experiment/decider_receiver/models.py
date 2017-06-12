fd945571d66f358e1213a1830dc40a5e29029fa4
Steve Tarzia <steve@stevetarzia.com>
2017-06-12 09:44:07 -0500

    renamed decisor_receptor to decider_receiver

--- my_experiment/decider_receiver/models.py
+++ my_experiment/decider_receiver/models.py
@@ -12,7 +12,7 @@ Your app description
 
 
 class Constants(BaseConstants):
-    name_in_url = 'decisor_receptor'
+    name_in_url = 'decider_receiver'
     players_per_group = None
     num_rounds = 1
 
