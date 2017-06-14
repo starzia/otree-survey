7cf5efd5282565132b87df95cc68c6fb6a7e6464
Steve Tarzia <steve@stevetarzia.com>
2017-06-14 17:15:03 -0500

    two-thirds of average game

--- my_experiment/two_thirds/models.py
+++ my_experiment/two_thirds/models.py
@@ -22,8 +22,19 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+    two_thirds = models.FloatField()
+
+    def set_payoffs(self):
+        self.two_thirds = (2.0/3) * sum([p.answer for p in self.get_players()])
+        min_error = min([abs(p.answer - self.two_thirds) for p in self.get_players()])
+        for p in self.get_players():
+            if abs(p.answer - self.two_thirds) == min_error:
+                p.payoff = p.max_payoff()
 
 
 class Player(BasePlayer):
-    pass
+    answer = models.IntegerField(min=0, max=100)
+
+    def max_payoff(self):
+        return c(10 if self.session.config["high_payment"] else 5);
+
